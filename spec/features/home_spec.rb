ed4b6ca28e39fc5e386dffbbb24bcf88c4bc6355
Josh Klina <joshua.klina@gmail.com>
2013-02-15 21:44:09 -0500

    passing no submissions scenario

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,19 +1,18 @@
 require 'spec_helper'
 
 describe "Home Page" do
-  before(:each) do
-    @submissions = FactoryGirl.create_list(:submission, 5)
-    visit root_url
-  end
   context "when there are no submissions" do
     it "should inform the user there are no submissions" do
+      visit root_url
       page.should have_content("There are no submissions yet!")
     end
   end
 
   context "where there are submissions" do
     it "should display the submissions" do
-      @submissions.each do |submission|
+      submissions = FactoryGirl.create_list(:submission, 5)
+      visit root_url
+      submissions.each do |submission|
         page.should have_content("#{submission.title}")
       end
     end
