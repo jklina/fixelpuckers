adf997d206d49784f22d28d6060b3d32d9555846
Josh Klina <joshua.klina@gmail.com>
2013-04-05 22:13:34 -0400

    starting on submission page

--- spec/features/submissions_spec.rb
+++ spec/features/submissions_spec.rb
@@ -1,11 +1,49 @@
 require 'spec_helper'
 
-# describe "Submissions" do
-#   describe "GET /submissions" do
-#     it "works! (now write some real specs)" do
-#       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#       get submissions_path
-#       response.status.should be(200)
-#     end
-#   end
-# end
+describe "Submission page" do
+  before (:each) do
+    @submission = FactoryGirl.create(:submission)
+    visit submission_path(@submission)
+  end
+
+  it "shows the submission's title" do
+    page.should have_content(@submission.title)
+  end
+
+  it "shows the submission's user's username" do
+    page.should have_content(@submission.user.username)
+  end
+
+  context "when there are reviews" do 
+    before (:each) do
+      @submission = FactoryGirl.create(:submission)
+    end
+
+    it "lists reviews" do
+      review = FactoryGirl.create(:review)
+      @submission.reviews << review
+      visit submission_path(@submission)
+      page.should have_content(review.comment)
+      page.should have_content(review.rating)
+    end
+  end
+
+  context "when a guest visits" do
+    before(:each) do
+      @submission = FactoryGirl.create(:submission)
+      visit submission_path(@submission)
+    end
+
+    it "allows the guest to access the submission" do
+      current_path.should == submission_path(@submission)
+    end
+
+    it "asks the user to sign up to leave a review" do
+      page.should have_content("Please sign up to leave a review.")
+    end
+
+    it "doesn't show a review form" do
+      page.should_not have_selector('form')
+    end
+  end
+end
