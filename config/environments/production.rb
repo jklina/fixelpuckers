115d6db8b944e992a3ed3537e8077d2ba9035c8d
Christian Reuter <christian@madewithnuance.com>
2013-04-08 21:21:58 -0400

    Add fonts path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Pf::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
