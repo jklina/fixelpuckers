7b3f677971f94231853fb8a4c981aa90bd9e5150
Josh Klina <joshua.klina@gmail.com>
2013-05-24 23:15:49 -0400

    moving user page scaffolding to userpage and adding the username

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
 describe "Viewing a user's page" do
-  it "should use friendly urls" do
+  it "uses friendly urls" do
     user = FactoryGirl.create(:user)
-    user.to_param.should eq(user.slug)
+    expect(user.to_param).to eq(user.slug)
+  end
+
+  it "displays the user's username" do
+    user = FactoryGirl.create(:user)
+    visit user_path(user)
+    expect(page).to have_content(user.username)
   end
 end
