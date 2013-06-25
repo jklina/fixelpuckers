d16a8b677c28b0c6c60b4d1a70bd1672fa7a5619
Josh Klina <joshua.klina@gmail.com>
2013-06-24 21:50:06 -0400

    reorganizing commenting system into a polymorphic model

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,19 +1,18 @@
 require 'spec_helper'
 
 describe "viewing a user" do
+  let(:user) { FactoryGirl.create(:user) }
+
   it "uses friendly urls" do
-    user = FactoryGirl.create(:user)
     expect(user.to_param).to eq(user.slug)
   end
 
   it "displays the user's username" do
-    user = FactoryGirl.create(:user)
     visit user_path(user)
     expect(page).to have_content(user.username)
   end
 
   it "displays the user's last three submissions" do
-    user = FactoryGirl.create(:user)
     submissions = FactoryGirl.create_list(:submission, 4, user: user)
     visit user_path(user)
     submissions.sort! { |a, b| b.created_at <=> a.created_at }
@@ -21,5 +20,10 @@ describe "viewing a user" do
       expect(page).to have_content(submission.title)
     end
     expect(page).not_to have_content(submissions.last.title)
+  end
+
+  it_behaves_like 'a commentable page' do
+    let(:commentable) { user }
+    let(:commentable_path) { user_path(user) }
   end
 end
