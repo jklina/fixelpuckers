53af9e715fa67ab9465ebc1fd68c6404d802342c
Josh Klina <joshua.klina@gmail.com>
2015-06-01 14:17:41 -0400

    adding tests for admin page

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -5,6 +5,21 @@ describe "Admin Page", type: :feature do
 
   it "has a sub nav with admin functions" do
     visit(admin_path(as: admin.id))
+
     expect(page).to have_selector("#admin-nav")
+  end
+
+  it "can be accessed by an admin" do
+    visit(root_path(as: admin.id))
+
+    expect(page).to have_content(I18n.t('home.index.menu.admin'))
+  end
+
+  it "cannot be accesed by a non admin" do
+    normal_user = create(:user)
+
+    visit(root_path(as: normal_user.id))
+
+    expect(page).to_not have_content(I18n.t('home.index.menu.admin'))
   end
 end
